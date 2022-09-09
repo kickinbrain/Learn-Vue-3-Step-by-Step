60cc7e33a502a15c577140fcb89ec032928224d3
kickinbrain <kickinbrain@gmail.com>
2022-09-09 15:17:43 +0400

    completed episode 6 - Component Props

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,13 +1,28 @@
 export default {
     template: `
-                <button class="bg-gray-200 text-gray px-5 py-2" :disabled="processing">
+                <button 
+                :class="{
+                    'text-gray px-5 py-2 m-4' : true,
+                    'bg-red-200' : type === 'danger',
+                    'bg-blue-200' : type === 'secondary',
+                    'bg-gray-200' : type === 'primary',
+                    'is-loading' : processing
+                   
+                }" 
+                :disabled="processing"
+                
+                >
                     <slot />
                 </button>
                 `,
-
-    data (){
-        return {
-            processing : false,
+    props: {
+        type: {
+            type: String,
+            default: 'primary'
+        },
+        processing:{
+            type: Boolean,
+            default: false
         }
     }
 }

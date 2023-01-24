020560fe827ea72327f5ec07f5802617e8a914dd
kickinbrain <kickinbrain@gmail.com>
2023-01-24 10:59:17 +0400

    completed episode 10 - It's All so Easy

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,11 +4,27 @@ export default {
     components: {Assignment},
     template: `
          <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
+            <h2 class="font-bold mb-2">
+                {{ title }}
+                <span>( {{ assignments.length }} )</span>
+            </h2>
+            
+            <div class="flex gap-2">
+                <button 
+                    @click="currentTag = tag"
+                    v-for="tag in tags" 
+                    class="rounded border px-1 py-px text-xs"
+                    :class="{
+                        'border-blue-500 text-blue-500' : tag === currentTag
+                    }"
+                    >
+                        {{ tag }}
+                </button>
+            </div>
     
-            <ul class="border border-gray-600 divide-y">
+            <ul class="border border-gray-600 divide-y mt-6">
                     <assignment
-                        v-for="assignment in assignments"
+                        v-for="assignment in filteredAssignments"
                         :key="assignment.id"
                         :assignment="assignment"
                     ></assignment>
@@ -19,5 +35,23 @@ export default {
     props: {
         assignments: Array,
         title: String,
+    },
+
+    data(){
+      return {
+          currentTag: ''
+      };
+    },
+
+    computed: {
+        filteredAssignments(){
+          if (this.currentTag === 'all'){
+              return this.assignments;
+          }
+          return this.assignments.filter(a => a.tag === this.currentTag);
+        },
+        tags(){
+            return ['all', ...new Set(this.assignments.map(a => a.tag))];
+        }
     }
 }

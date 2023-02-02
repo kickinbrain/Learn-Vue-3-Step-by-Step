d9e9b377cb2c61e58f303fb1cd7184d662113dd5
kickinbrain <kickinbrain@gmail.com>
2023-02-02 20:15:48 +0400

    completed episode 19 - From Mixins to Composables

---
+++ vite/vue-project/src/mixins/flash.js
@@ -0,0 +1,7 @@
+export default {
+    method: {
+        flash(message){
+            alert(message);
+        }
+    }
+}
--- vite/vue-project/src/views/AboutView.vue
+++ vite/vue-project/src/views/AboutView.vue
@@ -1,7 +1,14 @@
+<script>
+import flash from "../mixins/flash";
+
+export default {
+  mixins: [flash]
+}
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
     <button @click="flash('It Works on About page')">Click Me</button>
   </div>
 </template>
-

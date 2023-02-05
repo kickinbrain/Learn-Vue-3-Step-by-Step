c9329e7316470d46871e09a32a4c848a24471ceb
kickinbrain <kickinbrain@gmail.com>
2023-02-05 18:49:39 +0400

    completed episode 24 - Direct Mutation Concerns

--- vite/vue-project/src/App.vue
+++ vite/vue-project/src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from '@/components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/contact">Contact</RouterLink>
+        <RouterLink to="/count">Count</RouterLink>
       </nav>
     </div>
   </header>
